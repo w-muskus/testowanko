521ad5286850ad227d5404f8a0776ce1543cefe1
Wojciech Muskus <wojtekmuskus@gmail.com>
2021-01-11 11:50:30 +0100

    Update Tescik.java
    commit

--- Tescik/src/tescik/Tescik.java
+++ Tescik/src/tescik/Tescik.java
@@ -15,7 +15,7 @@ public class Tescik {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         System.out.print("Help!!!!!");
         System.out.print("Autobuild");
     }
